d6f73f6b3ccb5c3c369eca776da16d9d2de94317
mimimi32 <misesou2@gmail.com>
2026-04-23 08:50:37 +0900

    課題提出

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,12 +1,16 @@
 #include <cstdio>
+#include <omp.h>
 
-int main() {
+int main()
+{
   int n = 10;
   double dx = 1. / n;
   double pi = 0;
-  for (int i=0; i<n; i++) {
+#pragma omp parallel for reduction(+ : pi)
+  for (int i = 0; i < n; i++)
+  {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
   }
-  printf("%17.15f\n",pi);
+  printf("%17.15f\n", pi);
 }
